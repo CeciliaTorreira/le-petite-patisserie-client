407b09a1173423693a6fc6c2eb62fc09668a4d86
Cecilia Torreira <celia.umbreon@gmail.com>
2023-06-03 15:05:06 +0100

    Servicio par adquirir recetas favoritas del usuario activo funcionando perfectamente y se renderiza cuando pulso el botón para ello.

--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -4,6 +4,12 @@ const loadProfileService = () => {
 return service.get("/profile")  // Servicio para cargar el perfil de usuario que obtiene los datos del usuario activo
 }
 
+const getFavouriteRecipesService = () => {
+return service.get("/profile/favourite");  //Servicio para que se reciba la información de las rutas favoritas del usuario activo
+}
+export { loadProfileService, getFavouriteRecipesService}
 
 
-export { loadProfileService} 
+
+
+

5ac13dd2997c5e8eabbd1b8fb442966a7b7ec875
Cecilia Torreira <celia.umbreon@gmail.com>
2023-06-05 15:40:36 +0100

    Creación de comentarios funcionando, se añaden a la base de datos con sus respectivos Id de creador y receta correctamente, a la hora de volver a la receta y su lista de comentarios se visualiza correctamente

--- src/pages/comments/CommentList.jsx
+++ src/pages/comments/CommentList.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getRecipeCommentsService } from "../../services/recipe.services";
-import { useParams } from "react-router-dom";
-// import { ProgressBar } from  'react-loader-spi
+import { Link, useParams } from "react-router-dom";
+import { ProgressBar } from "react-loader-spinner";
+import { AuthContext } from "../../context/auth.context.js";
 
 function CommentList() {
   const [comments, setComments] = useState([]);
   const params = useParams();
-  //  const [isLoading, setIsLoading] =useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const {activeUser} = useContext(AuthContext)
+
 
   useEffect(() => {
     getData();
@@ -18,22 +21,42 @@ function CommentList() {
       const allComments = await getRecipeCommentsService(params.recipeId);
       console.log(allComments.data);
       setComments(allComments.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
     }
   };
 
+  if (isLoading) {
+    return (
+      <div>
+        <ProgressBar
+          height="80"
+          width="80"
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{}}
+          wrapperClass="progress-bar-wrapper"
+          borderColor="#51E5FF"
+          barColor="lightBlue"
+          className="loading-bar"
+        />
+      </div>
+    );
+  }
   return (
     <div>
       <h3>Comments and ratings</h3>
+{activeUser && <Link to={`/recipes/${params.recipeId}/comments/add`}><button className="buttons">Rate and comment this recipe</button></Link>} 
+      <hr />
       <section className="comments-list">
         {comments.map((eachComment) => {
           return (
             <div key={eachComment.id}>
-            <p>Written by: {eachComment.creator.username}</p>
+              <p>Written by: {eachComment.creator.username}</p>
               <li>Rating: {eachComment.rating}</li>
               <p>{eachComment.description}</p>
-               
+              <hr />
             </div>
           );
         })}
--- src/pages/comments/NewComment.jsx
+++ src/pages/comments/NewComment.jsx
@@ -1,9 +1,76 @@
-
+import { useState } from "react";
+import { createCommentService } from "../../services/recipe.services";
+import { useNavigate, useParams } from "react-router-dom";
 
 function NewComment() {
+  const params = useParams()
+  //ERROR
+  const [errorMessage, setErrorMessage] = useState("");
+
+  //FORM
+  const [description, setDescription] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const navigate = useNavigate();
+
+  const handleDescriptionChange = (e) => setDescription(e.target.value);
+  const handleRatingChange = (e) => setRating(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newComment = {
+        description,
+        rating,
+      };
+      await createCommentService(params.recipeId, newComment);
+
+      navigate("/profile");
+    } catch (error) {
+      if (error.response.status === 400) {
+        setErrorMessage(error.response.data.errorMessage);
+      } else {
+        navigate("/error");
+      }
+    }
+  };
+
   return (
-    <div>NewComment</div>
-  )
+    <div>
+      <h3>Share your opinion</h3>
+      <form onSubmit={handleSubmit}>
+        <br />
+        <label htmlFor="description"></label>
+        <br />
+        <textarea
+          className="comment-input"
+          type="text"
+          name="ingredients"
+          onChange={handleDescriptionChange}
+          value={description}
+        />
+        <br />
+        <label htmlFor="rating">Rating:</label> 
+        <input
+          type="number"
+          name="rating"
+          max={5}
+          onChange={handleRatingChange}
+          value={rating}
+        />
+        <br />
+        <button className="buttons" type="submit">
+          Post
+        </button>
+        {errorMessage && (
+          <p style={{ fontWeight: "bold", color: "darkblue" }}>
+            {errorMessage}
+          </p>
+        )}
+      </form>
+    </div>
+  );
 }
 
-export default NewComment
+export default NewComment;

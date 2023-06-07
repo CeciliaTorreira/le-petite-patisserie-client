2cd15e996a97c8715abc3166c0994eced47175f1
Cecilia Torreira <celia.umbreon@gmail.com>
2023-06-07 17:06:39 +0100

    Estilos arreglados para que la navbar se quede estática en cada vista

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,30 +1,35 @@
 import { useContext } from "react";
-import { AuthContext } from "../context/auth.context.js"
+import { AuthContext } from "../context/auth.context.js";
 import { useNavigate } from "react-router-dom";
-import {deleteCommentService} from "../services/recipe.services.js"
+import { deleteCommentService } from "../services/recipe.services.js";
 
-function CommentCard({eachComment, recipeId}) {
-const navigate = useNavigate()
-const {activeUser}= useContext(AuthContext)
+function CommentCard({ eachComment, recipeId }) {
+  const navigate = useNavigate();
+  const { activeUser } = useContext(AuthContext);
 
- const handleDelete = async () =>{
-
-try {
-  await deleteCommentService(recipeId, eachComment._id)
-  navigate(`/recipes/${recipeId}/`)
-} catch (error) {
-  navigate("/error")
-}
-
-}
+  const handleDelete = async () => {
+    try {
+      await deleteCommentService(recipeId, eachComment._id);
+      navigate(`/recipes/${recipeId}/`);
+    } catch (error) {
+      navigate("/error");
+    }
+  };
   return (
     <div className="comment-card">
-    <p>Rating: {eachComment.rating}</p>
-    <p>{eachComment.description}</p>
-     <p style={{fontWeight: "bold"}}>Written by: {eachComment.creator.username}</p>
-     {activeUser.role === "admin" && (<button onClick={handleDelete}>Delete comment</button>)} 
+      <p>
+        Written by:{" "}
+        <span style={{ fontWeight: "bold" }}>
+          {eachComment.creator.username}{" "}
+        </span>
+        || Rating: {eachComment.rating}
+        <br /><br />
+      <span className="comment-text">{eachComment.description}</span></p>
+      {activeUser && activeUser.role === "admin" && (
+        <button onClick={handleDelete}>Delete comment</button>
+      )}
     </div>
-  )
+  );
 }
 
-export default CommentCard
+export default CommentCard;
